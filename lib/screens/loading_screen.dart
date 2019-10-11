b43866d917524415ef73b2fe50bd8f11fbb62443
Ghaffaru <mudashiruagm@gmail.com>
2019-10-11 10:25:44 +0000

    fetched sample data from openweather map

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:clima/services/location.dart';
 
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
@@ -22,8 +23,25 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async {
-    Response response = await get('https://samples.openweathermap.org/data/2.5/heather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
-    print(response.statusCode);
+    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+//    print(response.statusCode);
+
+    if (response.statusCode == 200) {
+      
+      String data = response.body;
+      var decodedData = jsonDecode(data);
+
+      int condition = decodedData['weather'][0]['id'];
+      double temperature = decodedData['main']['temp'];
+      String city = decodedData['name'];
+
+      print(condition);
+      print(temperature);
+      print(city);
+
+    }else {
+      print(response.statusCode);
+    }
   }
 
   @override
