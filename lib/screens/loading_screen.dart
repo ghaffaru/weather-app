14c14e93f370756bca59f8e610a60d633cb4de24
Ghaffaru <mudashiruagm@gmail.com>
2019-10-11 09:50:58 +0000

    geolocation

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
 
-
-
+import 'package:http/http.dart';
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
@@ -10,18 +10,30 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
-  void getLocation() async {
-    Position position =
-    await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
+  @override
+  void initState() {
+    getLocation();
+    super.initState();
   }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+  }
+
+  void getData() async {
+    Response response = await get('https://samples.openweathermap.org/data/2.5/heather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    print(response.statusCode);
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            getLocation();
+//            getLocation();
           },
           child: Text('Get Location'),
         ),
